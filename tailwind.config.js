3be378f3175bd456f5bc75a2ef5406c08e607963
Willie Chalmers III <willie@williecubed.me>
2022-08-02 23:00:30 -0700

    fix(theme): Add missing Tailwind CSS styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,26 @@ module.exports = {
         primary: {
           DEFAULT: '#3C84FC',
         },
-      }
+        surface: {
+          DEFAULT: '#FFFFFF',
+          dark: '#64748b',
+        },
+        'on-surface': {
+          DEFAULT: '#000000',
+          dark: '#FAFAFA',
+        },
+        background: {
+          DEFAULT: '#F5F5F5',
+          dark: '#1e293b',
+        },
+      },
+    },
+    screens: {
+      sm: '600px',
+      md: '840px',
+      lg: '960px',
+      xl: '1240px',
+      '2xl': '1440px',
     },
   },
   plugins: [],
